31781eaf5f4c55389e1abe4bfe16f17c1a80a87a
Nour Kahile <65706592+nkahile@users.noreply.github.com>
2021-06-03 04:25:19 -0400

    Delete OPERATION4.sql

--- OPERATION4.sql
+++
@@ -1,9 +0,0 @@
-    CREATE FUNCTION Is_Eligible(@Student_Code INT) RETURNS CHAR AS
-    RETURN
-    DECLARE @result CHAR
-
-    IF(Select Is_Eligible FROM Pre_Exam) = 'Y'
-        RETURN 'True';
-    IF(@result = 'N')
-        RETURN 'False'
-END;
